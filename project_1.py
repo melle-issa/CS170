e8e4bd2fc40db3fcd5edc938434b4498c76e9845
ahern625 <ahern625@ucr.edu>
2023-05-04 18:52:47 -0700

    Worked on with Malina.
    Co-authored-by: mmart505 <mmart505@users.noreply.github.com>

--- project_1.py
+++ project_1.py
@@ -1,5 +1,7 @@
+
+
 #Student IDs:
-#Ailan Hernandez: 
+#Ailan Hernandez: 86
 #Melissa Hidalgo: 862211556
 #Lucyann Lacdan: 
 #Malina Martinez: 
@@ -15,6 +17,8 @@
 # 7 8 0
 
 #Node class
+
+
 class Node:
     def __init__(self, state, parent, operation, depth, cost):
         self.state = state
@@ -27,19 +31,19 @@ class Node:
 #parent class
 class Search_Alg:
     goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-    default_puzzle = [2, 3, 5, 4, 1, 8, 6, 0, 7]
+    default_puzzle = [1, 5, 0, 8, 3, 6, 4, 2, 7]
 
     def __init__(self) -> None:
         pass
 
     #prints the board
-    def print_board(curr_state):
+    def print_board(self, curr_state):
         print(curr_state[0], curr_state[1], curr_state[2])
         print(curr_state[3], curr_state[4], curr_state[5])
         print(curr_state[6], curr_state[7], curr_state[8])
 
-    def move_up(curr_state):
-        new_state = curr_state
+    def move_up(self, curr_state):
+        new_state = curr_state.copy()
         index = new_state.index(0)
         #check if the index of the empty space is at the top of the board
         if index not in [0, 1, 2]:
@@ -53,8 +57,8 @@ class Search_Alg:
         else: #if tile is on the top row, return an error
             return None
         
-    def move_down(curr_state):
-        new_state = curr_state
+    def move_down(self, curr_state):
+        new_state = curr_state.copy()
         index = new_state.index(0)
         if index not in [6, 7, 8]:
             temp = new_state[index + 3]
@@ -64,8 +68,8 @@ class Search_Alg:
         else:
             return None
         
-    def move_right(curr_state):
-        new_state = curr_state
+    def move_right(self, curr_state):
+        new_state = curr_state.copy()
         index = new_state.index(0)
         if index not in [2, 5, 8]:
             temp = new_state[index + 1]
@@ -75,8 +79,8 @@ class Search_Alg:
         else:
             return None
         
-    def move_left(curr_state):
-        new_state = curr_state
+    def move_left(self, curr_state):
+        new_state = curr_state.copy()
         index = new_state.index(0)
         if index not in [0, 3, 6]:
             temp = new_state[index - 1]
@@ -85,6 +89,23 @@ class Search_Alg:
             return new_state
         else:
             return None
+        
+    def trace(self, node):
+        path = []
+
+        while node.parent != None:
+            #path.append(node.operator)
+            path.append(node)
+            #self.print_board(node.state)
+            node = node.parent
+        path.reverse()
+
+        for i in range(len(path)):
+            print(path[i].operator)
+            self.print_board(path[i].state)
+
+        print("Total moves: ", len(path))
+
     
     def make_node(self, state, parent, operator, depth, cost):
         return Node(state, parent, operator, depth, cost)
@@ -107,6 +128,8 @@ class Search_Alg:
         node4 = self.move_right(input_node.state)
         if(node4 != None):
             options.append(self.make_node(node4, input_node, "move blank space right", input_node.depth + 1, 0))
+
+        return options
             
 
 #Uniform Cost Search
@@ -152,13 +175,63 @@ class Uniform_Cost(Search_Alg):
                     #replace node with it's lower cost route
                     fringe[fringe.index(node)] = node
 
-puzzle = Uniform_Cost()
-solution = puzzle.run(puzzle.default_puzzle)
-if solution != None:
-    print("Nodes expanded: ", solution[1])
-    print("Max in queue: ", solution[2])
+
+#A* Misplaced Tile Heuristic 
+class A_Misplaced(Search_Alg):
+    def getHeuristic(self, node):
+        heuristic = 0
+        for i in range(len(node.state)):
+            if node.state[i] != Search_Alg.goal_state[i]:
+                heuristic += 1
+        return heuristic
+
+    def run(self, start_state):
+        root_node = self.make_node(start_state, None, None, 0, 0)
+        queue = []
+        queue.append(root_node)
+        
+        
+        while queue:
+            minPos = 0
+            for i in range(len(queue)):
+                queue[i].totalCost = self.getHeuristic(queue[i]) + queue[i].depth
+                if(queue[i].totalCost < queue[minPos].totalCost):
+                    minPos = i
+            currNode = queue.pop(minPos)
+          
+            
+            if (currNode.state == self.goal_state):
+                return currNode
+            else:
+                expandedOptions = self.expand_node(currNode)
+                for option in expandedOptions:
+         
+                    if(option.state != currNode.state):
+                        queue.append(option)
+             
+                        
+        return None
+
+
+puzzleM = A_Misplaced()
+puzzleM.print_board(puzzleM.default_puzzle)
+solutionM=puzzleM.run(puzzleM.default_puzzle)
+if solutionM is not None:
+    puzzleM.trace(solutionM)
+else:
+    print("No solution found.")
+
+
+#puzzle = Uniform_Cost()
+#solution = puzzle.run(puzzle.default_puzzle)
+#if solution != None:
+    #print("Nodes expanded: ", solution[1])
+    #print("Max in queue: ", solution[2])
 
 #A* with Misplaced Tile Heuristic
+
+
+
 # class A_Star_Misplaced(Search_Alg):
 #     def run(self, start_state):
 #         fringe = []
