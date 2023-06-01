70ceaffbd2079332f5773c3c22e77d1d8a12f2a2
Melissa Hidalgo <mhida010@ucr.edu>
2023-05-31 19:50:09 -0700

    finished w small datasets

--- project_2.py
+++ project_2.py
@@ -4,18 +4,14 @@
 #Lucyann Lacdan: 862132856
 #Malina Martinez: 862311483
 
-#from numba import njit
-import random
 import pandas as pd
 import math
 import numpy as np
 
-#@njit
-
 class Classifier:
     def train(self, training_data_loc):
         #used https://www.geeksforgeeks.org/how-to-read-text-files-with-pandas/ for the following line
-        training = pd.read_csv(training_data_loc, delimiter='\s{1,2}', header=None, engine='python', skipinitialspace=True)
+        training = pd.read_csv(training_data_loc, delimiter='\s{2,3}', header=None, engine='python', skipinitialspace=True)
         training = training.rename(columns={0: "label"})
         return training
 
@@ -49,34 +45,25 @@ class Classifier:
 
 
 class Validator:
-    def leave_one_out_validation(data, features):
+    def leave_one_out_validation(self, data, features):
         numCorrect=0
         for i in range(len(data)):
             testingData=data.copy()
             testing_instance=testingData.iloc[i].values
             testingData = testingData.drop(testingData.index[i])
-            classificationTest = classify.test(testing_instance, features, testingData)
-            classificationActual = classify.test(testing_instance, features, data)
+            classificationTest = Classifier().test(testing_instance, features, testingData)
+            classificationActual = Classifier().test(testing_instance, features, data)
             if(classificationActual==classificationTest):
                 numCorrect=numCorrect+1
         return numCorrect/(len(data))
             
 
-        
-
-
-def leave_one_out_cross_validation(data, current_features, feature_to_add):
-    #For right now, returns a random number
-    rand = round(random.uniform(0, 100), 2)
-    return rand
-
-def forward_selection(num_features, data):
+def forward_selection(num_features, training_set):
+    data = training_set.copy()
     print("\nBeginning search\n")
     features = []
-    best_accuracy = leave_one_out_cross_validation(data, features, 0)
+    best_accuracy = 0.0
     final_features = []
-
-    print("Using no features and \"random\" evaluation, we get an accuracy of ", best_accuracy, "%\n")
 
     without_labels = data.drop(columns=['label'])
     for i in range(num_features):
@@ -85,8 +72,8 @@ def forward_selection(num_features, data):
         for k in without_labels.columns:
             if k not in features:
                 features.append(k)
-                accuracy = leave_one_out_cross_validation(data, features, k)
-                print(" Using feature(s) ", str(features), " accuracy is ", accuracy, "%")
+                accuracy = Validator().leave_one_out_validation(data, features)
+                print(" Using feature(s) ", str(features), " accuracy is ", accuracy*100, "%")
                 features.remove(k)
 
                 if accuracy > best_accuracy_in_loop:
@@ -95,23 +82,24 @@ def forward_selection(num_features, data):
 
         features.append(best_feature)
 
-        print("\nFeature set ", str(features), " was best with an accuracy of ", best_accuracy_in_loop, "%")
+        print("\nFeature set ", str(features), " was best with an accuracy of ", best_accuracy_in_loop*100, "%")
         if best_accuracy_in_loop > best_accuracy:
                     best_accuracy = best_accuracy_in_loop
                     final_features = features.copy()
         elif best_accuracy_in_loop < best_accuracy:
             print("(Warning. Accuracy has decreased!)")
 
-    print("\nFinished search!! The best subset is ", str(final_features), ", which has an accuracy of ", best_accuracy, "%")
+    print("\nFinished search!! The best subset is ", str(final_features), ", which has an accuracy of ", best_accuracy*100, "%")
 
-def backward_selection(data):
+def backward_selection(data, training_set):
+    training_copy = training_set.copy()
     print("Beginning search")
     currFeatures = []
     for i in range(data):
         currFeatures.append(i+1)
 
-    accuracy=leave_one_out_cross_validation(1,1,1)
-    print("\nWithout removing features, Feature set "+str(currFeatures)+ " accuracy is "+str(accuracy)+"%")
+    accuracy=Validator().leave_one_out_validation(training_copy, currFeatures)
+    print("\nWithout removing features, Feature set "+str(currFeatures)+ " accuracy is ", accuracy * 100, "%")
     highestAccuracy=accuracy
     bestFeatures=currFeatures.copy()
     
@@ -120,8 +108,8 @@ def backward_selection(data):
 
         lowestAccuracy=101
         for j in range(len(currFeatures)):
-            accuracy=leave_one_out_cross_validation(1,1,1)
-            print(" Removing feature(s) {"+str(currFeatures[j])+"} accuracy is "+str(accuracy)+"%")
+            accuracy=Validator().leave_one_out_validation(training_copy, currFeatures)
+            print(" Removing feature(s) {"+str(currFeatures[j])+"} accuracy is ", accuracy * 100, "%")
             if(accuracy<lowestAccuracy):
                 lowestAccuracy=accuracy
                 lowestAccuracyPos=j
@@ -131,47 +119,29 @@ def backward_selection(data):
                 bestFeatures=currFeatures.copy()
                 bestFeatures.pop(highestAccuracyPos)
         currFeatures.pop(lowestAccuracyPos)
-        print("\nFeature set "+str(currFeatures)+" was the worst, accuracy is "+str(lowestAccuracy)+"%")
+        print("\nFeature set "+str(currFeatures)+" was the worst, accuracy is ", lowestAccuracy * 100, "%")
 
         if highestAccuracy > accuracy:
             print("(Warning. Accuracy has decreased!)")
 
 
-    print("\nFinished search!! The best subset is "+str(bestFeatures)+", which has an accuracy of "+str(highestAccuracy)+"%")
+    print("\nFinished search!! The best subset is "+str(bestFeatures)+", which has an accuracy of ", highestAccuracy * 100, "%")
     
 
-#reads a made up data file for right now
-# data = pd.read_csv('data.txt', delimiter=' ', header=None) #this somehow still works with txt files lol
-# data = data.rename(columns={0: "label"})
 
-# print("Welcome to our Feature Selection Algorithm!\n")
-# inp = input("Please enter the total number of features: ")
-# print("\n1. Forward Selection")
-# print("2. Backward Selection")
-# algorithm_selection = input("\nPlease select an algorithm to run: ")
+print("Welcome to our Feature Selection Algorithm!\n")
+inp = "Small_data__52.txt" #input("Please enter the name of the text file (include extension): ")
 
-# if algorithm_selection == "1":
-#     forward_selection(int(inp), data)
+data = Classifier().train(inp)
 
-# else:
-#     #backward_selection(data)
-#     backward_selection(int(inp))
+print("\n1. Forward Selection")
+print("2. Backward Selection")
+algorithm_selection = input("\nPlease select an algorithm to run: ")
 
-#2.0000000e+000  3.7256854e+000  1.8995100e+000  4.0813166e+000  1.8676158e+000  3.2975607e+000  1.0805679e+000  2.7805422e+000  4.3284038e+000  2.7017165e+000  4.7808253e+000
+num_features = len(data.columns) - 1
 
-classify = Classifier()
-training_set = classify.train('small-test-dataset.txt')
-testing_instance = [3.7256855e+000,  1.8995100e+000,  4.0813166e+000,  1.8676158e+000,  3.2975607e+000,  1.0805679e+000,  2.7805422e+000,  4.3284038e+000,  2.7017165e+000,  4.7808253e+000]
-features = [1, 2, 3]
-classification = classify.test(testing_instance, features, training_set)
-print(classification)
+if algorithm_selection == "1":
+    forward_selection(num_features, data)
 
-
-features = [3, 5, 7]
-accuracy=Validator.leave_one_out_validation(training_set,features)
-print(f"Accuracy with features {', '.join(map(str, features))}: {accuracy}")
-
-#big_training_set = classify.train('large-test-dataset-1.txt')
-#features = [1, 15, 27]
-#accuracy=Validator.leave_one_out_validation(big_training_set,features)
-#print(f"Accuracy with features {', '.join(map(str, features))}: {accuracy}")
+else:
+    backward_selection(num_features, data)
